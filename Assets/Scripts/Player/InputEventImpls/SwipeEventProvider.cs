8f655d170668ada923ac9463e650371821223c19
Shoma SATO <noir.neo.04@gmail.com>
2018-04-15 19:09:27 +0900

    SwipeEventProvider 実装

---
+++ Assets/Scripts/Player/InputEventImpls/SwipeEventProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SwipeEventProvider : MonoBehaviour, IInputEventProvider
+{
+    [SerializeField] private Button button;
+
+    private IObservable<PointerEventData> OnDragAsObservable()
+    {
+        return button.OnDragAsObservable();
+    }
+
+    IObservable<Vector2> IInputEventProvider.MoveAsObservable()
+    {
+        return OnDragAsObservable()
+            .TakeUntilDestroy(this)
+            .Select(x => x.delta);
+    }
+}
