f5600298fd776e637997ad5d2f69121444e5ceea
Shoma SATO <noir.neo.04@gmail.com>
2018-04-15 19:12:19 +0900

    移動する

---
+++ Assets/Scripts/Extensions/Vector2Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Vector2Extensions
+{
+    public static Vector3 X0Y(this Vector2 vector2)
+    {
+        return new Vector3(vector2.x, 0, vector2.y);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private PlayerCore _core;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed;
+
+    void Start()
+    {
+        _core.MoveAsObservable()
+            .ObserveOn(Scheduler.MainThreadFixedUpdate)
+            .Subscribe(Move)
+            .AddTo(this);
+    }
+
+    void Move(Vector2 delta)
+    {
+        _rb.MovePosition(_rb.position + (delta * _speed * Time.deltaTime).X0Y());
+    }
+}
