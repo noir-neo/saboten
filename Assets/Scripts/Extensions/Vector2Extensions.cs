64a769a6db2dc9e0b10fbb94af8ba5503f63cdf4
Shoma SATO <noir.neo.04@gmail.com>
2018-05-04 19:30:01 +0900

    nav mesh agent で移動する

--- Assets/Scripts/Extensions/Vector2Extensions.cs
+++ Assets/Scripts/Extensions/Vector2Extensions.cs
@@ -1,7 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public static class Vector2Extensions
 {
+    public static Vector2 Sum(this IEnumerable<Vector2> source)
+    {
+        return source.Aggregate(Vector2.zero, (current, next) => current + next);
+    }
+
     public static Vector3 X0Y(this Vector2 vector2)
     {
         return new Vector3(vector2.x, 0, vector2.y);
--- Assets/Scripts/Player/InputEventImpls/SwipeEventProvider.cs
+++ Assets/Scripts/Player/InputEventImpls/SwipeEventProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
@@ -16,8 +17,9 @@ public class SwipeEventProvider : MonoBehaviour, IInputEventProvider
 
     IObservable<Vector2> IInputEventProvider.MoveAsObservable()
     {
-        return OnDragAsObservable()
-            .TakeUntilDestroy(this)
-            .Select(x => x.delta);
+        return this.UpdateAsObservable().Select(_ => Vector2.zero)
+            .Merge(OnDragAsObservable().Select(x => x.delta))
+            .BatchFrame()
+            .Select(x => x.Sum());
     }
 }
